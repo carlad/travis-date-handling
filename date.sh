942b7ac064845efe9903e60cbe7a8803e1b3777f
carlad <carlad@users.noreply.github.com>
2017-02-15 15:49:32 +0100

    more sleep, dodgy date format

--- date.sh
+++ date.sh
@@ -2,27 +2,49 @@
 set -e
 
 sudo date -s '2016-08-09 11:59:00' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:20' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:21' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:22' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:23' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:24' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:25' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:26' '+%F %T'
+echo "date is now " $(date)
 sudo date -s '2016-08-09 11:59:27' '+%F %T'
-sudo date -s '2016-08-09 11:59:28' '+%F %T'
-sudo date -s '2016-08-09 11:59:29' '+%F %T'
-sudo date -s '2016-08-09 11:59:30' '+%F %T'
-sudo date -s '2016-08-09 11:59:31' '+%F %T'
-sudo date -s '2016-08-09 11:59:32' '+%F %T'
-sudo date -s '2016-08-09 11:59:33' '+%F %T'
-sudo date -s '2016-08-09 11:59:34' '+%F %T'
-sudo date -s '2016-08-09 11:59:35' '+%F %T'
-sudo date -s '2016-08-09 11:59:36' '+%F %T'
-sudo date -s '2016-08-09 11:59:37' '+%F %T'
-sudo date -s '2016-08-09 11:59:38' '+%F %T'
-sudo date -s '2016-08-09 11:59:39' '+%F %T'
+echo "date is now " $(date)
 sleep 3
+sudo date -s '2016-08-09 11:59:28' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:29' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:30' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:31' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:00059:32:000000' '+%F %T'
+echo "date is now " $(date)
+
+sudo date -s '2016-08-09 11:59:33' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:34' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:35' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:36' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:37' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:38' '+%F %T'
+echo "date is now " $(date)
+sudo date -s '2016-08-09 11:59:39' '+%F %T'
+sleep 5
 sudo date -s '2016-08-09 11:59:40' '+%F %T'
 echo "date is now " $(date)
 
